c868fa06a0c68163b2ac1567a5901139df9a15c9
Patrick R <mail@patrickrombouts.nl>
2022-01-25 09:39:13 +0100

    Add page contents

---
+++ src/Printdeal.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types = 1);
+
+namespace Printdeal;
+
+class Printdeal
+{
+    
+    function careers()
+    {
+        Header("Location: https://careers.printdeal.com/");
+    }
+}
